e09f173e263b8c63a93aea470b9bcf3c60ca4ae7
Jordan Yu <jordan.yu@punchh.com>
2019-08-15 16:27:30 -0700

    Build out components

---
+++ vue_personal_portfolio/src/components/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+
+<section id="sidebar">
+  <div class="inner">
+    <nav>
+      <ul>
+        <li><a href="#intro">Welcome</a></li>
+        <li><a href="#one">Recent projects</a></li>
+        <li><a href="#two">Technologies</a></li>
+        <li><a href="#three">Hardware prototyping</a></li>
+        <li><a href="#four">Get in touch</a></li>
+      </ul>
+    </nav>
+  </div>
+</section>
+
+</template>
+
+<script>
+
+export default {
+  name: "sidebar",
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  methods: {}
+}
+
+</script>
