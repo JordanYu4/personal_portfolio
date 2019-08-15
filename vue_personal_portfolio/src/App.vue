ec6f70cce9f8c7e417da7e3a9d1c86dd7693669f
Jordan Yu <jordan.yu@punchh.com>
2019-08-15 16:41:22 -0700

    Import homepage components

--- vue_personal_portfolio/src/App.vue
+++ vue_personal_portfolio/src/App.vue
@@ -2,17 +2,39 @@
   <div id="app">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Sidebar />
+
+    <div id="wrapper">
+      <Intro />      
+      <Projects />
+      <Skills />
+      <HardwarePrototypingSplash />
+      <Contact />
+    </div>
+    
+    <Footer />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Sidebar from './components/Sidebar.vue'
+import Sidebar from './components/Sidebar.vue';
+import Intro from './components/Intro.vue';
+import Projects from './components/Projects.vue';
+import Skills from './components/Skills.vue';
+import HardwarePrototypingSplash from './components/HardwarePrototypingSplash.vue';
+import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue'; 
 
 export default {
   name: 'app',
   components: {
-    Sidebar
+    Sidebar,
+    Intro,
+    Projects,
+    Skills,
+    HardwarePrototypingSplash,
+    Contact,
+    Footer
   } 
 }
 </script>
