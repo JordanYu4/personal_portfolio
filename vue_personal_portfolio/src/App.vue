1abe2cdd93eea57263f90e6a7c95bfa8f8468c75
Jordan Yu <jordan.yu@punchh.com>
2019-08-15 16:27:03 -0700

    Begin building out custom components

--- vue_personal_portfolio/src/App.vue
+++ vue_personal_portfolio/src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Sidebar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Sidebar
+  } 
 }
 </script>
 
 <style>
+@import "../../assets/css/main.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
